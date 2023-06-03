aae0848a9a369930c9905ee96676a6b4e9d2a0b6
Antony Jackson <antony.jackson.Bestmate71@gmail.com>
2023-06-03 10:47:33 +0100

    Adding test project

---
+++ src/main/java/com/udacity/timezones/service/TimeZoneService.java
@@ -0,0 +1,19 @@
+package com.udacity.timezones.service;
+
+import com.udacity.timezones.client.WorldTimeApiClient;
+
+public class TimeZoneService {
+	private final WorldTimeApiClient worldTimeApiRestClient;
+
+	public TimeZoneService(WorldTimeApiClient worldTimeApiRestClient) {
+		this.worldTimeApiRestClient = worldTimeApiRestClient;
+	}
+
+	public String getAvailableTimezoneText(String area) {
+		return String.format(
+			"Available timezones in %s are %s.",
+			area,
+			String.join(", ", worldTimeApiRestClient.getValidTimeZones(area))
+		);
+	}
+}
